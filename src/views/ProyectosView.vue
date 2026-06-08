630d64c37b64243806294401d965507fb46ad985
Enrique-Mora-Zavala <L21390359@chetumal.tecnm.mx>
2026-06-07 21:36:30 -0500

    Se desarollo el CRUD para gestionar los proyectos

--- src/views/ProyectosView.vue
+++ src/views/ProyectosView.vue
@@ -1,5 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+import ProyectoForm from '../components/proyectos/ProyectoForm.vue'
+import ProyectosList from '../components/proyectos/ProyectosList.vue'
+
+const actualizarLista = ref(0)
+const proyectoSeleccionado = ref(null)
+
+const recargarProyectos = () => {
+  actualizarLista.value++
+  proyectoSeleccionado.value = null
+}
+
+const editarProyecto = (proyecto) => {
+  proyectoSeleccionado.value = proyecto
+}
+</script>
+
 <template>
   <div>
     <h1>Proyectos</h1>
+
+    <ProyectoForm :proyectoEditar="proyectoSeleccionado" @proyecto-guardado="recargarProyectos" />
+
+    <hr />
+
+    <ProyectosList :key="actualizarLista" @editar-proyecto="editarProyecto" />
   </div>
 </template>
