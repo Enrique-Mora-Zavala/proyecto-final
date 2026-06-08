42d3cb77a86d58a8d9a37cce679bff7ec60bfa0e
Enrique-Mora-Zavala <L21390359@chetumal.tecnm.mx>
2026-06-07 19:15:57 -0500

    Se desarrolla el router para que se conecte con las vistas

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import HomeView from '../views/HomeView.vue'
+import AlumnosView from '../views/AlumnosView.vue'
+import ProyectosView from '../views/ProyectosView.vue'
+import ParticipacionesView from '../views/ParticipacionesView.vue'
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/alumnos',
+      name: 'alumnos',
+      component: AlumnosView,
+    },
+    {
+      path: '/proyectos',
+      name: 'proyectos',
+      component: ProyectosView,
+    },
+    {
+      path: '/participaciones',
+      name: 'participaciones',
+      component: ParticipacionesView,
+    },
+  ],
 })
 
 export default router
