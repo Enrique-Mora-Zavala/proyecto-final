328b1afaa347232f25522875614bc6b92b86223e
Enrique-Mora-Zavala <L21390359@chetumal.tecnm.mx>
2026-06-07 19:03:46 -0500

    Se crearon las vistas de la aplicación

---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Sistema de Gestión de Proyectos Académicos</h1>
+  </div>
+</template>
---
+++ src/views/ParticipacionesView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Participaciones</h1>
+  </div>
+</template>
